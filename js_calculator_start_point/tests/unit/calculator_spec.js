63eb61165d2279ac0b6ad396f71fdfc4bafdb829
Caroline Reid <carolinehreid@gmail.com>
2017-12-08 12:02:35 +0000

    adds clearClick passing test

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -35,10 +35,62 @@ describe('calculator', function () {
     assert.equal(calculator.runningTotal, 8);
   })
 
-  it('should reset the newTotal when a number is clicked', function(){
+  it('should reset the runningTotal when a number is clicked', function(){
     calculator.runningTotal = 10;
     calculator.numberClick(4);
     assert.equal(calculator.runningTotal, 4)
   })
+
+  it('should perform the add operation if the previous operation was add', function(){
+    calculator.numberClick(8);
+    calculator.operatorClick('+');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    assert.equal(calculator.runningTotal, 10);
+  })
+
+  it('should perform the subtract operation if the previous operation was subtract', function(){
+    calculator.numberClick(8);
+    calculator.operatorClick('-');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    assert.equal(calculator.runningTotal, 6);
+  })
+
+  it('should perform the multiply operation if the previous operation was multiply', function(){
+    calculator.numberClick(8);
+    calculator.operatorClick('*');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    assert.equal(calculator.runningTotal, 16);
+  })
+
+  it('should perform the divide operation if the previous operation was divide', function(){
+    calculator.numberClick(8);
+    calculator.operatorClick('/');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    assert.equal(calculator.runningTotal, 4);
+  })
+
+  it('should perform the correct operator when equals is pressed', function(){
+    calculator.previousOperator = '+';
+    calculator.operatorClick('=');
+    assert.equal(calculator.previousOperator, null);
+  })
+
+  it('should update total', function(){
+    calculator.previousTotal = 10;
+    calculator.runningTotal = 12;
+    assert.equal(calculator.newTotal, true);
+  })
+
+  it('should clear the running total', function(){
+    calculator.runningTotal = 10;
+    calculator.clearClick();
+    assert.equal(calculator.runningTotal, 0);
+  })
+
+
 
 });
